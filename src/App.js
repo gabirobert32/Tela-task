bd00a0c75991161114b4ba34131d7a274331fdf6
Gabriel Robert <gabirobert@gmail.com>
2022-07-08 10:08:14 -0300

    Teladoc task commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import {useEffect, useState} from 'react';
 import './App.scss';
 import Species from './Species';
+import axios from 'axios';
 
-const API_URL = 'https://swapi.dev/api/films/2/';
+const API_URL_TESB = 'https://swapi.dev/api/films/2/';
+
 const SPECIES_IMAGES = {
   droid:
     'https://static.wikia.nocookie.net/starwars/images/f/fb/Droid_Trio_TLJ_alt.png',
@@ -16,10 +19,57 @@ const SPECIES_IMAGES = {
 const CM_TO_IN_CONVERSION_RATIO = 2.54;
 
 function App() {
+  const [filmTESB, setTESB] = useState();
+
+  useEffect(() => {
+    axios
+      .get(API_URL_TESB)
+      .then(resTESB => {
+        const promises = [];
+        resTESB.data.species.forEach(item => {
+          promises.push(axios.get(item));
+        });
+        return Promise.all(promises);
+      })
+      .then(data => {
+        setTESB(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  const heightCalculator = height => {
+    var heightInches;
+    if (isNaN(parseInt(height)) || typeof parseInt(height) !== 'number') {
+      heightInches = 'N/A';
+    } else {
+      heightInches = (parseInt(height) / CM_TO_IN_CONVERSION_RATIO).toFixed(2);
+      return `${Math.round(heightInches)}"`;
+    }
+    return heightInches;
+  };
+
   return (
     <div className="App">
       <h1>Empire Strikes Back - Species Listing</h1>
-      <div className="App-species" />
+      <div className="App-species">
+        {filmTESB &&
+          filmTESB.map((specie, index) => (
+            <Species
+              key={index}
+              name={specie.data.name}
+              classification={specie.data.classification}
+              designation={specie.data.designation}
+              height={heightCalculator(specie.data.average_height)}
+              image={
+                SPECIES_IMAGES[specie.data.name.split("'")[0].toLowerCase()]
+              }
+              numFilms={specie.data.films.length}
+              language={specie.data.language}
+            />
+          ))}
+      </div>
     </div>
   );
 }
